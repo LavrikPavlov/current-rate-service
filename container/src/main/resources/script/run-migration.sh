7778e971a4e0a874bcd3a6a08436d2f065e216c5
mihailkazan <misha-kazan2013@yandex.ru>
2024-12-25 05:12:17 +0700

    task-6: add other currency, ref script

--- container/src/main/resources/script/run-migration.sh
+++ container/src/main/resources/script/run-migration.sh
@@ -1,2 +1,21 @@
 #!/bin/bash
-mvn liquibase:update
+# Запуск миграции на БД
+
+OS=$(uname)
+
+if [[ "$OS" == "Darwin" ]]; then
+  # macOS
+  readonly MAVEN_HOME="/Applications/IntelliJ IDEA.app/Contents/plugins/maven/lib/maven3/bin"
+elif [[ "$OS" == "MINGW"* || "$OS" == "CYGWIN"* || "$OS" == "MSYS"* ]]; then
+  # Windows
+  readonly MAVEN_HOME="C:/Program Files/JetBrains/IntelliJ IDEA/plugins/maven/lib/maven3/bin"
+else
+  echo "Операционная система не поддерживается."
+  exit 1
+fi
+
+echo "Операционная система: $OS"
+
+cd "$(dirname "$0")/../../../.." || exit
+
+"$MAVEN_HOME/mvn" liquibase:update
--- service/src/main/java/ru/kazan/currencyrateservice/service/ClientService.java
+++ service/src/main/java/ru/kazan/currencyrateservice/service/ClientService.java
@@ -1,6 +1,7 @@
 package ru.kazan.currencyrateservice.service;
 
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 public interface ClientService {
 
